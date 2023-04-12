7560ea3e47b34fe85b58342f9a50571f07e92b32
Vishnu Mali <vishnumali3911@gmail.com>
2023-04-12 14:39:36 +0530

    form upload

--- src/pages/ProductForm.js
+++ src/pages/ProductForm.js
@@ -7,55 +7,24 @@ const BACKEND_URI = 'http://localhost:4002/'
 const ProductForm = () => {
   const [name, setName] = useState("");
   const [description, setdescription] = useState("");
-  const [price,setPrice]=useState("");
+  const [price, setPrice] = useState("");
   const [cover, setCover] = useState();
   const [image, setimage] = useState();
   const [video, setVideos] = useState();
-
+  const config = { headers: { 'Content-Type': 'multipart/form-data' } }
 
   const hadleSubmit = (e) => {
     e.preventDefault();
 
-    let formdata = new FormData();
-    // for (var i = 0; i < video.length; i++) {
-    //   formdata.append("video", video[i]);
-    // }
-    // for (var j = 0; j < image.length; j++) {
-    //   formdata.append("image", image[j]);
-    // }
-    // const formdata = new FormData();
-    formdata.append("name", name);
-    formdata.append("description", description);
-    formdata.append("price", price)
-    formdata.append("image", video);
-    formdata.append("video",image);
-     axios.post(`${BACKEND_URI}user/addproduct`, formdata, { headers: {'Content-Type': 'multipart/form-data'}}).then((data)=>{
+    let formdata = new FormData(e.target);
+    axios.post(`${BACKEND_URI}user/addproduct`, formdata, { headers: { 'Content-Type': 'multipart/form-data' } }).then((data) => {
       alert("Submitted successfully");
       return data
-  }).catch((error)=>{
-    alert("error happened");
+    }).catch((error) => {
+      alert("error happened");
       return error
-  })
-    
+    })
 
-    
-
-    // var payload = {}
-    // formdata.forEach((key, value) => {
-    //   console.log(value, key);
-    //   payload[value] = key
-    // })
-    // console.log(payload)
-    // axios
-    //   .post(`${BACKEND_URI}user/addproduct`, payload)
-    //   .then((success) => {
-
-    //     alert("Submitted successfully");
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //     alert("Error happened!");
-    //   });
   };
 
   return (
@@ -72,30 +41,30 @@ const ProductForm = () => {
               name="name"
               id="name"
               className="form-control"
-              onChange={(e) => setName(e.target.value)}
+            // onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="form-group">
-            <label htmlFor="description">descriptionription</label>
+            <label htmlFor="description">description</label>
             <input
               type="text"
               name="description"
               id="description"
               className="form-control"
-              onChange={(e) => setdescription(e.target.value)}
+            // onChange={(e) => setdescription(e.target.value)}
             />
           </div>
           <div className="form-group">
-          <label htmlFor="name">Price</label>
-          <input
-            type="number"
-            name="price"
-            id="price"
-            className="form-control"
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </div>
-        
+            <label htmlFor="name">Price</label>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              className="form-control"
+            // onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+
 
           <div className="form-group">
             <label htmlFor="video">Upload Videos</label>
@@ -106,9 +75,9 @@ const ProductForm = () => {
               // multiple
               className="form-control"
               accept=".mp4, .mkv"
-              onChange={(e) => {
-                setVideos(e.target.files);
-              }}
+            // onChange={(e) => {
+            //   setVideos(e.target.files);
+            // }}
             />
           </div>
           <div className="form-group">
@@ -120,9 +89,9 @@ const ProductForm = () => {
               multiple
               className="form-control"
               accept=".jpg, .png,.jpeg,.webp"
-              onChange={(e) => {
-                setimage(e.target.files);
-              }}
+            // onChange={(e) => {
+            //   setimage(e.target.files);
+            // }}
             />
           </div>
 
